ed5d0c46fab0ea5a8841c5e678acd0320f4902ed
ABHINAV SUREKA <7731608+ABHINAV-SUREKA@users.noreply.github.com>
2022-05-27 21:38:48 +0530

    making server more robust

--- internal/app/server.go
+++ internal/app/server.go
@@ -4,11 +4,17 @@ import (
 	"fmt"
 	log "github.com/sirupsen/logrus"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 // Run starts HTTP server, listens to, and services the incoming user requests
 func (appCfg *AppConfig) Run() {
+	stopChan := make(chan os.Signal, 1) // create a channel to receive interrupts & shut down the server gracefully
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
 	srv := http.Server{
 		Addr:         fmt.Sprintf(":%d", appCfg.ServerConfig.Port),
 		Handler:      appCfg.Routes(),
@@ -18,5 +24,15 @@ func (appCfg *AppConfig) Run() {
 	}
 
 	log.Infof("Starting server on port %v...", appCfg.ServerConfig.Port)
-	log.Fatal(srv.ListenAndServe())
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			log.Errorf("error starting server: %s", err.Error())
+			stopChan <- syscall.SIGTERM
+		}
+	}()
+
+	<-stopChan
+	log.Info("Server shut down gracefully")
 }
