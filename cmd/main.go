7071d233f142222c8bb44dc895453f172114eba9
ABHINAV SUREKA <7731608+ABHINAV-SUREKA@users.noreply.github.com>
2022-06-10 23:55:23 +0530

    enhance error

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ func main() {
 	flag.DurationVar(&srvCfg.IdleTimeout, "idle-timeout", constants.IdleTimeout, "Maximum no. of seconds to wait for the next request when keep-alive is enabled")
 	flag.DurationVar(&srvCfg.ReadTimeout, "read-timeout", constants.ReadTimeout, "Maximum no. of seconds before timing out reading of entire request, including the body")
 	flag.DurationVar(&srvCfg.WriteTimeout, "write-timeout", constants.WriteTimeout, "Maximum no. of seconds before timing out writing of the response")
-	flag.StringVar(&appCfg.JWT.SecretKey, "jwt-secret-key", "", "JWT secret key for signing token") // TODO: provide a secret key (say, a HMAC encrypted one) via cmd line arg
+	flag.StringVar(&appCfg.JWT.SecretKey, "jwt-secret-key", "", "Secret key for signing JWT") // TODO: provide a secret key (say, a HMAC encrypted one) via cmd line arg
 	flag.Parse()
 	appCfg.ServerConfig = srvCfg
 
