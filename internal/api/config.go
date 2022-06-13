937db9ff8d5104f9ef7297776112920eed13b037
ABHINAV SUREKA <7731608+ABHINAV-SUREKA@users.noreply.github.com>
2022-06-13 19:56:00 +0530

    adding interface{}

--- internal/api/config.go
+++ internal/api/config.go
@@ -1,18 +1,16 @@
 package api
 
-import "github.com/ABHINAV-SUREKA/go-backend-server-base-code/internal/db"
-
 // Config interface declaring functions for api/graphql operations
 type Config interface { // we are exposing api.Config interface & its any required function outside the current package
 }
 
 // config struct implements Config interface methods
 type config struct {
-	dbConfig *db.Config // creating dbConfig here coz api config receiver functions call db config receiver functions
+	dbConfig interface{} // creating dbConfig here coz api config receiver functions call db config receiver functions
 }
 
 // New creates new api config struct
-func New(dbConfig *db.Config) Config {
+func New(dbConfig interface{}) Config {
 	return &config{
 		dbConfig: dbConfig,
 	}
